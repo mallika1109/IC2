f2402379607dd8222825c460e2d4702ef8e69655
mallika1109 <mallikamacharla1997@gmail.com>
2025-07-16 15:52:49 -0400

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,15 @@
 const request = require('supertest');
-const app = require('../src/app');
+const { app, server } = require('../src/app');
 
 describe('GET /', () => {
   it('should return Hello, World!', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World!');
+  });
+
+  // Close the server after all tests
+  afterAll((done) => {
+    server.close(done);
   });
 });
